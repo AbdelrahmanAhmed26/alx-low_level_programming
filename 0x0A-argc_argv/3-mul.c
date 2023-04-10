32deec71e2d1c29e50067acb90999db2753deaa6
boody <bd2642001@gmail.com>
2023-04-10 07:55:18 -0700

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
 
 
 int main(int argc, char *argv[])
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 
 
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
 int main(int argc, char *argv[])
 {
 	int num, digit, sum = 0;
